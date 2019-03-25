67bba4e9f7e2a868b70d6345c10ef91b7054578f
whitemata <trabajospflc@outlook.com>
2019-03-24 20:49:44 -0700

    B1.0.1

--- EstructuraV-1.0/EstructuraV-1.0/Form1.cs
+++ EstructuraV-1.0/EstructuraV-1.0/Form1.cs
@@ -16,5 +16,45 @@ namespace EstructuraV_1._0
         {
             InitializeComponent();
         }
+
+        private void btnBlanco_MouseEnter(object sender, EventArgs e)
+        {
+            btnBlanco.BackColor = Color.FromArgb(226, 204, 0); // CAMBIAR COLOR AL PASAR MOUSE POR ENCIMA
+        }
+
+        private void btnBlanco_MouseLeave(object sender, EventArgs e)
+        {
+            btnBlanco.BackColor = Color.FromArgb(40, 45, 54); // AL NO ESTAR MOUSE ENCIMA SE CAMBIA EL COLOR
+        }
+
+        private void btnLeopoldo_MouseEnter(object sender, EventArgs e)
+        {
+            btnLeopoldo.BackColor = Color.FromArgb(226, 204, 0); // CAMBIAR COLOR AL PASAR MOUSE POR ENCIMA
+        }
+
+        private void btnLeopoldo_MouseLeave(object sender, EventArgs e)
+        {
+            btnLeopoldo.BackColor = Color.FromArgb(40, 45, 54); // AL NO ESTAR MOUSE ENCIMA SE CAMBIA EL COLOR
+        }
+
+        private void btnAlberto_MouseEnter(object sender, EventArgs e)
+        {
+            btnAlberto.BackColor = Color.FromArgb(226, 204, 0); // CAMBIAR COLOR AL PASAR MOUSE POR ENCIMA
+        }
+
+        private void btnAlberto_MouseLeave(object sender, EventArgs e)
+        {
+            btnAlberto.BackColor = Color.FromArgb(40, 45, 54); // AL NO ESTAR MOUSE ENCIMA SE CAMBIA EL COLOR
+        }
+
+        private void btnAlonso_MouseEnter(object sender, EventArgs e)
+        {
+            btnAlonso.BackColor = Color.FromArgb(226, 204, 0); // CAMBIAR COLOR AL PASAR MOUSE POR ENCIMA
+        }
+
+        private void btnAlonso_MouseLeave(object sender, EventArgs e)
+        {
+            btnAlonso.BackColor = Color.FromArgb(40, 45, 54); // AL NO ESTAR MOUSE ENCIMA SE CAMBIA EL COLOR
+        }
     }
 }
